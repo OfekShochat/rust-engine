6203386247adb5717bc0a4a04a3d1a334a42f8bf
Ofek Shochat <o@shoch.at>
2021-05-16 17:45:34 +0300

    a lot of changes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-mod search;
+mod uci;
 extern crate chess;
-use std::str::FromStr;
-use std::io::stdin;
 
 #[allow(unused_must_use)]
 fn main() {
-  let mut searcher = search::Search::new();
-  loop {
-    let mut s:String = String::new();
-    stdin().read_line(&mut s);
-    let board: chess::Board = chess::Board::from_str(&s.to_string()).unwrap();
-    let result: (i32, chess::ChessMove) = searcher.iterative_deepening(board, 100, -10000, 10000);
-    println!("bestmove {}", result.1);
-  }
+  let mut uci_handle = uci::UciFunctions::new();
+  uci_handle.go(100, -1, -1, false);
 }
