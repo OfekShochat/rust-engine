abf01296f1c5c14c4bc61f603bb692813093a07c
Ofek Shochat <o@shoch.at>
2021-09-30 11:43:56 +0300

    starting again

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::time::Instant;
+
 extern crate chess;
 
+mod search;
+
 fn main() {
-  
+  let mut s = search::SearchWorker::new();
+  let start = Instant::now();
+  let r = s.search(chess::Board::default(), -10000, 10000, 10);
+  println!(
+    "cp {} nps {} time {}",
+    r,
+    s.nodes as f32 / start.elapsed().as_secs_f32(),
+    start.elapsed().as_secs_f32()
+  );
 }
