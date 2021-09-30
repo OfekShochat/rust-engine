59ef93c71f788d9afd1f4a1a6a5ee4ddc9574c4e
Ofek Shochat <o@shoch.at>
2021-09-30 09:03:52 +0300

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-mod uci;
-pub mod search;
-pub mod sthread;
 extern crate chess;
 
-#[allow(unused_must_use)]
 fn main() {
-  let mut uci_handle = uci::UciFunctions::new();
-  uci_handle.go(100, -1, -1, false);
+  
 }
